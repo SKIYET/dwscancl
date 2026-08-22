8ce7ceb081fb99eac37cebe7989eb98fe14be5ef
CNMan <nklijl@mail.nankai.edu.cn>
2026-08-22 13:00:03 +0000

    Sat Aug 22 01:00:03 PM UTC 2026

--- 6.keyupdate.cmd
+++ 6.keyupdate.cmd
@@ -1,3 +1,3 @@
 @echo off
-wget --append-output=keyupdatelog.txt --no-verbose -N --no-if-modified-since http://st.drweb.com/static/beta_files/win/12.5/keys/drweb32.key
+wget --append-output=keyupdatelog.txt --no-verbose -N --no-if-modified-since https://st.drweb.com/static/beta_files/win/12.5/key/drweb32.key
 pause
